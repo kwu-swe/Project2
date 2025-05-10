92962b7bd86566c37c380f365e3359a00abf5989
hann <wjdgks1233@gmail.com>
2025-05-11 01:02:21 +0900

    refactor : add print

--- main.c
+++ main.c
@@ -41,6 +41,7 @@ void filetime2() {}
 
 // 두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp() {
+  printf("size compare\n");
   struct stat stat1, stat2;
 
     if (stat("text1", &stat1) == -1) {
@@ -54,16 +55,17 @@ void sizecmp() {
 
 
     if (stat1.st_size > stat2.st_size) {
-        printf("text1 is bigger\n");
+        printf("text1 is bigger\n\n");
     } else if (stat1.st_size < stat2.st_size) {
-        printf("text2 is bigger\n");
+        printf("text2 is bigger\n\n");
     } else {
-        printf("sizes are equal\n");
+        printf("sizes are equal\n\n");
     }
 }
 
 // 두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp() {
+  printf("block compare\n");
   struct stat stat1, stat2;
 
     if (stat("text1", &stat1) == -1) {
@@ -76,11 +78,11 @@ void blockcmp() {
     }
 
     if (stat1.st_blocks > stat2.st_blocks) {
-        printf("text1 is bigger\n");
+        printf("text1 is bigger\n\n");
     } else if (stat1.st_blocks < stat2.st_blocks) {
-        printf("text2 is bigger\n");
+        printf("text2 is bigger\n\n");
     } else {
-        printf("sizes are equal\n");
+        printf("sizes are equal\n\n");
     }
 }
 
